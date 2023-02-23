f8d005dc9fee879f884541e41fe46c5f4713bd06
Alejandro Visiedo <avisiedo@redhat.com>
2023-02-23 11:32:54 +0100

    datamodel:clean-up comments
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/domain/model/ipa.go
+++ internal/domain/model/ipa.go
@@ -4,24 +4,6 @@ import "gorm.io/gorm"
 
 // See: https://gorm.io/docs/models.html
 
-/*
-{
-  "domain_uuid": "1aa15eae-a88b-11ed-a2cb-482ae3863d30",
-  "domain_name": "mydomain.example",
-  "domain_type": "ipa",
-  "auto_enrollment_enabled": true,
-  "ipa": {
-    "realm_name": "IPA.EXAMPLE",
-    "ca_list": "base64",
-    "server_list": [
-      "server1.mydomain.example",
-      "Server2.mydomain.example"
-    ],
-    "client_options": {}
-  }
-}
-*/
-
 type Ipa struct {
 	gorm.Model
 	DomainID  uint
