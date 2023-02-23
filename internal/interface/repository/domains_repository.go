e583d32a4adb6583377f7a010ec8f8b21b4396a7
Alejandro Visiedo <avisiedo@redhat.com>
2023-02-23 17:36:41 +0100

    repository:refactor domains repository component
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/interface/repository/domains_repository.go
+++ internal/interface/repository/domains_repository.go
@@ -6,10 +6,10 @@ import (
 )
 
 type DomainRepository interface {
-	FindAll(db *gorm.DB, offset int64, count int32) (output []model.Domain, err error)
-	Create(db *gorm.DB, data *model.Domain) (err error)
-	// PartialUpdate(db *gorm.DB, data *model.Domain) (output model.Domain, err error)
-	// Update(db *gorm.DB, data *model.Domain) (output model.Domain, err error)
-	FindById(db *gorm.DB, id uint) (output model.Domain, err error)
-	DeleteById(db *gorm.DB, id uint) (err error)
+	FindAll(db *gorm.DB, orgId string, offset int64, count int32) (output []model.Domain, err error)
+	Create(db *gorm.DB, orgId string, data *model.Domain) (err error)
+	// PartialUpdate(db *gorm.DB, orgId string, data *model.Domain) (output model.Domain, err error)
+	// Update(db *gorm.DB, orgId string, data *model.Domain) (output model.Domain, err error)
+	FindById(db *gorm.DB, orgId string, uuid string) (output model.Domain, err error)
+	DeleteById(db *gorm.DB, orgId string, uuid string) (err error)
 }
--- internal/usecase/repository/domain_repository.go
+++ internal/usecase/repository/domain_repository.go
@@ -14,29 +14,30 @@ func NewDomainRepository() repository.DomainRepository {
 	return &domainRepository{}
 }
 
-func (r *domainRepository) FindAll(db *gorm.DB, offset int64, limit int32) (data []model.Domain, err error) {
+func (r *domainRepository) FindAll(db *gorm.DB, orgId string, offset int64, count int32) (output []model.Domain, err error) {
 	if db == nil {
 		return []model.Domain{}, fmt.Errorf("db is nil")
 	}
 	if offset < 0 {
 		return []model.Domain{}, fmt.Errorf("offset lower than 0")
 	}
-	if limit < 0 {
+	if count < 0 {
 		return []model.Domain{}, fmt.Errorf("limit lower than 0")
 	}
-	if err = db.Offset(int(offset)).Limit(int(limit)).Find(&data).Error; err != nil {
+	if err = db.Offset(int(offset)).Limit(int(count)).Find(&output).Error; err != nil {
 		return []model.Domain{}, err
 	}
 	return
 }
 
-func (r *domainRepository) Create(db *gorm.DB, data *model.Domain) (err error) {
+func (r *domainRepository) Create(db *gorm.DB, orgId string, data *model.Domain) (err error) {
 	if db == nil {
 		return fmt.Errorf("db is nil")
 	}
 	if data == nil {
 		return fmt.Errorf("data is nil")
 	}
+	data.OrgId = orgId
 	if err = db.Create(data).Error; err != nil {
 		return err
 	}
@@ -45,63 +46,86 @@ func (r *domainRepository) Create(db *gorm.DB, data *model.Domain) (err error) {
 
 func (r *domainRepository) getColumnsToUpdate(data *model.Domain) []string {
 	cols := []string{"id"}
-	if data.Title != nil {
-		cols = append(cols, "title")
+	if data.AutoEnrollmentEnabled != nil {
+		cols = append(cols, "auto_enrollment_enabled")
 	}
-	if data.Description != nil {
-		cols = append(cols, "description")
+	if data.DomainName != nil {
+		cols = append(cols, "domain_name")
+	}
+	if data.DomainType != nil {
+		cols = append(cols, "domain_type")
 	}
 	return cols
 }
 
-func (r *domainRepository) PartialUpdate(db *gorm.DB, data *model.Domain) (output model.Domain, err error) {
-	if db == nil {
-		return model.Domain{}, fmt.Errorf("db is nil")
-	}
-	if data == nil {
-		return model.Domain{}, fmt.Errorf("data is nil")
-	}
-	cols := r.getColumnsToUpdate(data)
-	if err = db.Model(data).Select(cols).Updates(*data).Error; err != nil {
-		return model.Domain{}, err
-	}
-	return *data, nil
-}
+// func (r *domainRepository) PartialUpdate(db *gorm.DB, orgId string, data *model.Domain) (output model.Domain, err error) {
+// 	if db == nil {
+// 		return model.Domain{}, fmt.Errorf("db is nil")
+// 	}
+// 	if data == nil {
+// 		return model.Domain{}, fmt.Errorf("data is nil")
+// 	}
+// 	data.OrgId = orgId
+// 	cols := r.getColumnsToUpdate(data)
+// 	if err = db.Model(data).Select(cols).Updates(*data).Error; err != nil {
+// 		return model.Domain{}, err
+// 	}
+// 	return *data, nil
+// }
 
-func (r *domainRepository) Update(db *gorm.DB, data *model.Domain) (output model.Domain, err error) {
-	if db == nil {
-		return model.Domain{}, fmt.Errorf("db is nil")
-	}
-	if data == nil {
-		return model.Domain{}, fmt.Errorf("data is nil")
-	}
-	cols := []string{"id"}
-	if err = db.Model(data).Select(cols).Updates(*data).Error; err != nil {
-		return model.Domain{}, err
-	}
-	return *data, nil
-}
+// func (r *domainRepository) Update(db *gorm.DB, orgId string, data *model.Domain) (output model.Domain, err error) {
+// 	if db == nil {
+// 		return model.Domain{}, fmt.Errorf("db is nil")
+// 	}
+// 	if data == nil {
+// 		return model.Domain{}, fmt.Errorf("data is nil")
+// 	}
+// 	data.OrgId = orgId
+// 	cols := []string{"id"}
+// 	if err = db.Model(data).Select(cols).Updates(*data).Error; err != nil {
+// 		return model.Domain{}, err
+// 	}
+// 	return *data, nil
+// }
 
-func (r *domainRepository) FindById(db *gorm.DB, id uint) (output model.Domain, err error) {
+// See: https://gorm.io/docs/query.html
+// TODO Document the method
+func (r *domainRepository) FindById(db *gorm.DB, orgId string, uuid string) (output model.Domain, err error) {
+	var count int64
 	if db == nil {
 		return model.Domain{}, fmt.Errorf("db is nil")
 	}
-	if err = db.First(&output, int(id)).Error; err != nil {
+	if err = db.Count(&count).First(&output, "org_id = ? AND domain_uuid = ?", orgId, uuid).Error; err != nil {
 		return model.Domain{}, err
 	}
 	return output, nil
 }
 
-func (r *domainRepository) DeleteById(db *gorm.DB, id uint) (err error) {
-	var data model.Domain
+func (r *domainRepository) DeleteById(db *gorm.DB, orgId string, uuid string) (err error) {
+	var (
+		data  model.Domain
+		count int64
+	)
 	if db == nil {
 		return fmt.Errorf("db is nil")
 	}
-	if err = db.First(&data, id).Error; err != nil {
+	if orgId == "" {
+		return fmt.Errorf("orgId cannot be an empty string")
+	}
+	if uuid == "" {
+		return fmt.Errorf("uuid cannot be an empty string")
+	}
+	if err = db.Count(&count).First(&data, "org_id = ? AND domain_uuid = ?", orgId, uuid).Error; err != nil {
 		return err
 	}
-	if err = db.Delete(&data, id).Error; err != nil {
+	if count == 1 {
+		return fmt.Errorf("Register not found")
+	}
+	if err = db.Count(&count).Delete(&data, "org_id = ? AND domain_uuid = ?", orgId, uuid).Error; err != nil {
 		return err
+	}
+	if count == 0 {
+		return fmt.Errorf("None register was deleted")
 	}
 	return nil
 }
