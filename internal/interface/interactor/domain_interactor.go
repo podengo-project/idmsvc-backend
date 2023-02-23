7f2e76ac7079e6dcf3b2e9682ccfe5c253d90429
Alejandro Visiedo <avisiedo@redhat.com>
2023-02-23 16:12:33 +0100

    interactor:refactor and align with the interface and the gorm domain
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/interface/interactor/domain_interactor.go
+++ internal/interface/interactor/domain_interactor.go
@@ -1,13 +1,14 @@
 package interactor
 
 import (
+	"github.com/hmsidm/internal/api/public"
 	api_public "github.com/hmsidm/internal/api/public"
 	"github.com/hmsidm/internal/domain/model"
 )
 
 type DomainInteractor interface {
-	Create(params *api_public.CreateDomainParams, body *api_public.CreateDomain, out *model.Domain) error
-	Delete(id string, params *api_public.DeleteDomainParams, out *string) error
-	List(params *api_public.ListDomainsParams, offset *int64, limit *int32) error
-	GetById(params string, out *string) error
+	Create(params *api_public.CreateDomainParams, body *api_public.CreateDomain) (string, *model.Domain, error)
+	Delete(uuid string, params *api_public.DeleteDomainParams) (string, string, error)
+	List(params *api_public.ListDomainsParams) (orgId string, offset int, limit int, err error)
+	GetById(uuid string, params *public.ReadDomainParams) (string, string, error)
 }
--- internal/usecase/interactor/domain_interactor.go
+++ internal/usecase/interactor/domain_interactor.go
@@ -2,7 +2,9 @@ package interactor
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/hmsidm/internal/api/public"
 	api_public "github.com/hmsidm/internal/api/public"
 	"github.com/hmsidm/internal/domain/model"
 	"github.com/hmsidm/internal/interface/interactor"
@@ -11,37 +13,53 @@ import (
 
 type domainInteractor struct{}
 
-func NewTodoInteractor() interactor.DomainInteractor {
+func NewDomainInteractor() interactor.DomainInteractor {
 	return domainInteractor{}
 }
 
+func helperDomainTypeToUint(domainType public.CreateDomainDomainType) uint {
+	switch domainType {
+	case public.CreateDomainDomainTypeIpa:
+		return model.DomainTypeIpa
+	default:
+		return model.DomainTypeUndefined
+	}
+}
+
 // TODO Document method
-func (i domainInteractor) Create(params *api_public.CreateDomainParams, in *api_public.CreateDomain, out *model.Domain) (error) {
+func (i domainInteractor) Create(params *api_public.CreateDomainParams, body *api_public.CreateDomain) (string, *model.Domain, error) {
 	if params == nil {
-		return fmt.Errorf("'params' cannot be nil")
+		return "", nil, fmt.Errorf("'params' cannot be nil")
 	}
-	if in == nil {
-		return fmt.Errorf("'in' cannot be nil")
-	}
-	if out == nil {
-		return fmt.Errorf("'out' cannot be nil")
+	if body == nil {
+		return "", nil, fmt.Errorf("'body' cannot be nil")
 	}
 	// FIXME Add a middleware that decode the X-Rh-Identity and store
 	//       the structure into the request context, so we can use directly
 	//       into the specific service handler and pass the information to
 	//       the interactors
+
+	domain := &model.Domain{}
 	identity, err := DecodeIdentity(string(params.XRhIdentity))
 	if err != nil {
-		return err
+		return "", nil, err
 	}
-	out.OrgId = identity.OrgID
-	out.DomainName = pointy.String(in.DomainName)
-	switch in.DomainType {
-	case api_public.CreateDomainDomainTypeIpa:
-		out.DomainType = pointy.Uint(model.DomainTypeIpa)
+	domain.OrgId = identity.OrgID
+	domain.AutoEnrollmentEnabled = pointy.Bool(body.AutoEnrollmentEnabled)
+	domain.DomainName = pointy.String(body.DomainName)
+	domain.DomainType = pointy.Uint(helperDomainTypeToUint(body.DomainType))
+
+	domain.IpaDomain = &model.Ipa{}
+	if body.Ipa.RealmName != nil {
+		domain.IpaDomain.RealmName = pointy.String(*body.Ipa.RealmName)
 	}
-	out.AutoEnrollmentEnabled = pointy.Bool(in.AutoEnrollmentEnabled)
-	return nil
+	if body.Ipa.ServerList != nil {
+		domain.IpaDomain.ServerList = pointy.String(strings.Join(*body.Ipa.ServerList, ","))
+	}
+	if body.Ipa.CaList != "" {
+		domain.IpaDomain.CaList = pointy.String(body.Ipa.CaList)
+	}
+	return identity.OrgID, domain, nil
 }
 
 // func (i domainInteractor) PartialUpdate(id public.Id, params *api_public.PartialUpdateTodoParams, in *api_public.Todo, out *model.Todo) error {
@@ -87,39 +105,45 @@ func (i domainInteractor) Create(params *api_public.CreateDomainParams, in *api_
 // }
 
 // TODO Document method
-func (i domainInteractor) Delete(id string, params *api_public.DeleteDomainParams, out *string) error {
+func (i domainInteractor) Delete(uuid string, params *api_public.DeleteDomainParams) (string, string, error) {
 	if params == nil {
-		return fmt.Errorf("'params' cannot be nil")
+		return "", "", fmt.Errorf("'params' cannot be nil")
 	}
-	if out == nil {
-		return fmt.Errorf("'out' cannot be nil")
+	identity, err := DecodeIdentity(string(params.XRhIdentity))
+	if err != nil {
+		return "", "", err
 	}
-	*out = id
-	return nil
+	return identity.OrgID, uuid, nil
 }
 
 // TODO Document method
-func (i domainInteractor) List(params *api_public.ListDomainsParams, offset *int, limit *int) error {
+func (i domainInteractor) List(params *api_public.ListDomainsParams) (orgId string, offset int, limit int, err error) {
 	if params == nil {
-		return fmt.Errorf("'in' cannot be nil")
+		return "", -1, -1, fmt.Errorf("'params' cannot be nil")
 	}
-	if offset == nil {
-		return fmt.Errorf("'offset' cannot be nil")
+	if params.Offset == nil {
+		return "", -1, -1, fmt.Errorf("'offset' cannot be nil")
 	}
-	if limit == nil {
-		return fmt.Errorf("'limit' cannot be nil")
+	if params.Limit == nil {
+		return "", -1, -1, fmt.Errorf("'limit' cannot be nil")
 	}
-	*offset = *params.Offset
-	*limit = *params.Limit
-	return nil
+	identity, err := DecodeIdentity(string(params.XRhIdentity))
+	if err != nil {
+		return "", -1, -1, err
+	}
+	return identity.OrgID, *params.Offset, *params.Limit, nil
 }
 
 // TODO Document method
-func (i domainInteractor) GetById(params *api_public.ReadDomainParams, in *string) (string, string, error) {
-	if in == nil {
-		return "", "", fmt.Errorf("'in' cannot be nil")
+func (i domainInteractor) GetById(uuid string, params *public.ReadDomainParams) (string, string, error) {
+	if uuid == "" {
+		return "", "", fmt.Errorf("'in' cannot be an empty string")
 	}
-	params.
 
-	return nil
+	identity, err := DecodeIdentity(string(params.XRhIdentity))
+	if err != nil {
+		return "", "", err
+	}
+
+	return identity.OrgID, uuid, nil
 }
