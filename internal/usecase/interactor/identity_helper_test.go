4788b5c9b3f33bfe2b117ca1c85e06bcdb6a79cc
Alejandro Visiedo <avisiedo@redhat.com>
2023-03-06 18:27:43 +0100

    test:enhance coverage for identity helpers into interactor components
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/usecase/interactor/identity_helper_test.go
+++ internal/usecase/interactor/identity_helper_test.go
@@ -33,3 +33,53 @@ func TestEncodeDecodeIdentity(t *testing.T) {
 	require.NotNil(t, id2)
 	assert.Equal(t, *id1, *id2)
 }
+
+func TestEncodeIdentity(t *testing.T) {
+	var result string
+
+	result = EncodeIdentity(nil)
+	assert.Equal(t, "", result)
+
+	result = EncodeIdentity(&identity.Identity{
+		OrgID:                 "12345",
+		AccountNumber:         "12345",
+		EmployeeAccountNumber: "12345",
+		Type:                  "User",
+		AuthType:              "basic-auth",
+		Internal: identity.Internal{
+			OrgID:       "12345",
+			CrossAccess: false,
+		},
+	})
+}
+
+func TestDecodeIdentity(t *testing.T) {
+	var (
+		result *identity.Identity
+		err    error
+	)
+
+	result, err = DecodeIdentity("")
+	assert.Nil(t, result)
+	require.Error(t, err)
+	assert.EqualError(t, err, "X-Rh-Identity content cannot be an empty string")
+
+	result, err = DecodeIdentity("abc")
+	assert.Nil(t, result)
+	require.Error(t, err)
+	assert.EqualError(t, err, "illegal base64 data at input byte 0")
+
+	result, err = DecodeIdentity("ewo=")
+	assert.Nil(t, result)
+	require.Error(t, err)
+	assert.EqualError(t, err, "unexpected end of JSON input")
+
+	result, err = DecodeIdentity(EncodeIdentity(&identity.Identity{
+		Internal: identity.Internal{
+			OrgID: "12345",
+		},
+	}))
+	assert.NotNil(t, result)
+	require.NoError(t, err)
+	assert.Equal(t, "12345", result.Internal.OrgID)
+}
