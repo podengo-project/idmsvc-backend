1a4afd4fd3fa1a4ad683903ebd37483984485856
Alejandro Visiedo <avisiedo@redhat.com>
2023-03-09 20:18:22 +0100

    api:add 'domain_description' for POST /domains endpoint
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/api/public/types.gen.go
+++ internal/api/public/types.gen.go
@@ -44,6 +44,9 @@ type CreateDomain struct {
 	// AutoEnrollmentEnabled Enable or disable host vm auto-enrollment for this domain
 	AutoEnrollmentEnabled bool `json:"auto_enrollment_enabled"`
 
+	// DomainDescription Humand readable description for this domain.
+	DomainDescription string `json:"domain_description"`
+
 	// DomainName Domain name
 	DomainName string `json:"domain_name"`
 
