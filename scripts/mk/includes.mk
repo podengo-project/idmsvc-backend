f0cd5f50650c524e170d38d5dd4d686f7c59f881
Alejandro Visiedo <avisiedo@redhat.com>
2023-03-27 14:40:37 +0200

    chore: clean-up commented includes
    - Remove the commented lines related with not used makefile files.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- scripts/mk/includes.mk
+++ scripts/mk/includes.mk
@@ -26,13 +26,11 @@ include scripts/mk/help.mk
 include scripts/mk/meta-general.mk
 include scripts/mk/gojsonschema.mk
 include scripts/mk/yq.mk
-# include scripts/mk/gen-api.mk
 include scripts/mk/go-rules.mk
 include scripts/mk/plantuml.mk
 include scripts/mk/api-doc.mk
 include scripts/mk/db.mk
 include scripts/mk/printvars.mk
-# include scripts/mk/swag.mk
 include scripts/mk/lint.mk
 include scripts/mk/meta-docker.mk
 include scripts/mk/docker.mk
@@ -40,8 +38,6 @@ include scripts/mk/meta-compose.mk
 include scripts/mk/compose.mk
 include scripts/mk/meta-kafka.mk
 include scripts/mk/kafka.mk
-# include scripts/mk/meta-repos.mk
-# include scripts/mk/repos.mk
 include scripts/mk/meta-ephemeral.mk
 include scripts/mk/ephemeral.mk
 
