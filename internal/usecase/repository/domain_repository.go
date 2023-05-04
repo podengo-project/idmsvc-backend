1ddefb6f784b5dbdb637244458d48640e1c52665
Alejandro Visiedo <avisiedo@redhat.com>
2023-05-04 09:51:57 +0200

    style(repository): minor style updates
    - Rename OrgId to OrgID.
    - Restrict line size to 80 chars but the mocked sql statements.
    - Move public methods at the beginning of the file, and private methods later.
    - Consistency on the errors returned to be aligned with previous changes.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/usecase/repository/domain_repository.go
+++ internal/usecase/repository/domain_repository.go
@@ -18,15 +18,20 @@ func NewDomainRepository() repository.DomainRepository {
 	return &domainRepository{}
 }
 
-func (r *domainRepository) FindAll(db *gorm.DB, orgId string, offset int64, count int32) (output []model.Domain, err error) {
+func (r *domainRepository) FindAll(
+	db *gorm.DB,
+	orgID string,
+	offset int64,
+	count int32,
+) (output []model.Domain, err error) {
 	if db == nil {
-		return []model.Domain{}, fmt.Errorf("db is nil")
+		return []model.Domain{}, fmt.Errorf("'db' is nil")
 	}
 	if offset < 0 {
-		return []model.Domain{}, fmt.Errorf("offset lower than 0")
+		return []model.Domain{}, fmt.Errorf("'offset' lower than 0")
 	}
 	if count < 0 {
-		return []model.Domain{}, fmt.Errorf("limit lower than 0")
+		return []model.Domain{}, fmt.Errorf("'limit' lower than 0")
 	}
 	err = db.Offset(int(offset)).Limit(int(count)).Find(&output).Error
 	if err != nil {
@@ -35,37 +40,19 @@ func (r *domainRepository) FindAll(db *gorm.DB, orgId string, offset int64, coun
 	return
 }
 
-func (r *domainRepository) createIpaDomain(db *gorm.DB, domainID uint, data *model.Ipa) (err error) {
-	if data == nil {
-		return fmt.Errorf("'data' of type '*model.Ipa' is nil")
-	}
-	data.Model.ID = domainID
-	tokenExpiration := &time.Time{}
-	*tokenExpiration = time.Now().Add(model.DefaultTokenExpiration()).UTC()
-	data.TokenExpiration = tokenExpiration
-	if err = db.Omit(clause.Associations).Create(data).Error; err != nil {
+func (r *domainRepository) Create(
+	db *gorm.DB,
+	orgID string,
+	data *model.Domain,
+) (err error) {
+	if err = r.checkCommonAndDataAndType(
+		db,
+		orgID,
+		data,
+	); err != nil {
 		return err
 	}
-	for idx := range data.CaCerts {
-		data.CaCerts[idx].IpaID = data.ID
-		if err = db.Create(&data.CaCerts[idx]).Error; err != nil {
-			return err
-		}
-	}
-	for idx := range data.Servers {
-		data.Servers[idx].IpaID = data.ID
-		if err = db.Create(&data.Servers[idx]).Error; err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (r *domainRepository) Create(db *gorm.DB, orgId string, data *model.Domain) (err error) {
-	if err = r.checkCommonAndDataAndType(db, orgId, data); err != nil {
-		return err
-	}
-	data.OrgId = orgId
+	data.OrgId = orgID
 	if err = db.Omit(clause.Associations).
 		Create(data).Error; err != nil {
 		return err
@@ -75,7 +62,11 @@ func (r *domainRepository) Create(db *gorm.DB, orgId string, data *model.Domain)
 		if data.IpaDomain == nil {
 			return nil
 		}
-		if err = r.createIpaDomain(db, data.ID, data.IpaDomain); err != nil {
+		if err = r.createIpaDomain(
+			db,
+			data.ID,
+			data.IpaDomain,
+		); err != nil {
 			return err
 		}
 		return nil
@@ -103,11 +94,11 @@ func (r *domainRepository) Create(db *gorm.DB, orgId string, data *model.Domain)
 // data for the current organization.
 func (r *domainRepository) Update(
 	db *gorm.DB,
-	orgId string,
+	orgID string,
 	data *model.Domain,
 ) (err error) {
 	var currentDomain *model.Domain
-	if err = r.checkCommonAndData(db, orgId, data); err != nil {
+	if err = r.checkCommonAndData(db, orgID, data); err != nil {
 		return err
 	}
 
@@ -115,7 +106,7 @@ func (r *domainRepository) Update(
 	// Check the entity exists
 	if currentDomain, err = r.FindById(
 		db,
-		orgId,
+		orgID,
 		uuid,
 	); err != nil {
 		return err
@@ -146,7 +137,7 @@ func (r *domainRepository) Update(
 	}
 
 	if err = db.Omit(clause.Associations).
-		Where("org_id = ? AND domain_uuid = ?", orgId, currentDomain.DomainUuid).
+		Where("org_id = ? AND domain_uuid = ?", orgID, currentDomain.DomainUuid).
 		Updates(data).
 		Error; err != nil {
 		return err
@@ -165,62 +156,18 @@ func (r *domainRepository) Update(
 	}
 }
 
-func (r *domainRepository) updateIpaDomain(db *gorm.DB, data *model.Ipa) (err error) {
-	if data == nil {
-		return fmt.Errorf("'data' of type '*model.Ipa' is nil")
-	}
-	if err = db.Unscoped().
-		Delete(
-			data,
-			"id = ?",
-			data.ID,
-		).Error; err != nil {
-		return err
-	}
-
-	// Clean token and create new entry
-	// data.Model.CreatedAt = time.Time{}
-	// data.Model.UpdatedAt = time.Time{}
-	// data.Model.DeletedAt = gorm.DeletedAt{}
-	data.Token = nil
-	data.TokenExpiration = nil
-	if err = db.Omit(clause.Associations).
-		Create(data).
-		Error; err != nil {
-		return err
-	}
-
-	// CaCerts
-	for i := range data.CaCerts {
-		data.CaCerts[i].IpaID = data.ID
-		if err = db.Create(&data.CaCerts[i]).Error; err != nil {
-			return err
-		}
-	}
-
-	// Servers
-	for i := range data.Servers {
-		data.Servers[i].IpaID = data.ID
-		if err = db.Create(&data.Servers[i]).Error; err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 // See: https://gorm.io/docs/query.html
 // TODO Document the method
 func (r *domainRepository) FindById(
 	db *gorm.DB,
-	orgId string,
+	orgID string,
 	uuid string,
 ) (output *model.Domain, err error) {
-	if err = r.checkCommonAndUUID(db, orgId, uuid); err != nil {
+	if err = r.checkCommonAndUUID(db, orgID, uuid); err != nil {
 		return nil, err
 	}
 	if err = db.Model(&model.Domain{}).
-		First(&output, "org_id = ? AND domain_uuid = ?", orgId, uuid).
+		First(&output, "org_id = ? AND domain_uuid = ?", orgID, uuid).
 		Error; err != nil {
 		return nil, err
 	}
@@ -247,35 +194,43 @@ func (r *domainRepository) FindById(
 }
 
 // See: https://gorm.io/docs/delete.html
-func (r *domainRepository) DeleteById(db *gorm.DB, orgId string, uuid string) (err error) {
+func (r *domainRepository) DeleteById(
+	db *gorm.DB,
+	orgID string,
+	uuid string,
+) (err error) {
 	var (
 		data  model.Domain
 		count int64
 	)
-	if err = r.checkCommonAndUUID(db, orgId, uuid); err != nil {
+	if err = r.checkCommonAndUUID(db, orgID, uuid); err != nil {
 		return err
 	}
-	if err = db.First(&data, "org_id = ? AND domain_uuid = ?", orgId, uuid).Count(&count).Error; err != nil {
+	if err = db.First(&data, "org_id = ? AND domain_uuid = ?", orgID, uuid).Count(&count).Error; err != nil {
 		return err
 	}
 	if count == 0 {
 		return fmt.Errorf("Register not found")
 	}
-	if err = db.Unscoped().Delete(&data, "org_id = ? AND domain_uuid = ?", orgId, uuid).Error; err != nil {
+	if err = db.Unscoped().Delete(&data, "org_id = ? AND domain_uuid = ?", orgID, uuid).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (r *domainRepository) RhelIdmClearToken(db *gorm.DB, orgId string, uuid string) (err error) {
-	if err = r.checkCommonAndUUID(db, orgId, uuid); err != nil {
+func (r *domainRepository) RhelIdmClearToken(
+	db *gorm.DB,
+	orgID string,
+	uuid string,
+) (err error) {
+	if err = r.checkCommonAndUUID(db, orgID, uuid); err != nil {
 		return err
 	}
 	var dataDomain *model.Domain
-	if dataDomain, err = r.FindById(db, orgId, uuid); err != nil {
+	if dataDomain, err = r.FindById(db, orgID, uuid); err != nil {
 		return err
 	}
-	if dataDomain.OrgId != orgId {
+	if dataDomain.OrgId != orgID {
 		return fmt.Errorf("'OrgId' mistmatch")
 	}
 	switch *dataDomain.Type {
@@ -298,18 +253,26 @@ func (r *domainRepository) RhelIdmClearToken(db *gorm.DB, orgId string, uuid str
 }
 
 // ------- PRIVATE METHODS --------
-func (r *domainRepository) checkCommon(db *gorm.DB, orgId string) error {
+
+func (r *domainRepository) checkCommon(
+	db *gorm.DB,
+	orgID string,
+) error {
 	if db == nil {
 		return fmt.Errorf("'db' is nil")
 	}
-	if orgId == "" {
-		return fmt.Errorf("'orgId' is empty")
+	if orgID == "" {
+		return fmt.Errorf("'orgID' is empty")
 	}
 	return nil
 }
 
-func (r *domainRepository) checkCommonAndUUID(db *gorm.DB, orgId string, uuid string) error {
-	if err := r.checkCommon(db, orgId); err != nil {
+func (r *domainRepository) checkCommonAndUUID(
+	db *gorm.DB,
+	orgID string,
+	uuid string,
+) error {
+	if err := r.checkCommon(db, orgID); err != nil {
 		return err
 	}
 	if uuid == "" {
@@ -318,8 +281,12 @@ func (r *domainRepository) checkCommonAndUUID(db *gorm.DB, orgId string, uuid st
 	return nil
 }
 
-func (r *domainRepository) checkCommonAndData(db *gorm.DB, orgId string, data *model.Domain) error {
-	if err := r.checkCommon(db, orgId); err != nil {
+func (r *domainRepository) checkCommonAndData(
+	db *gorm.DB,
+	orgID string,
+	data *model.Domain,
+) error {
+	if err := r.checkCommon(db, orgID); err != nil {
 		return err
 	}
 	if data == nil {
@@ -328,12 +295,89 @@ func (r *domainRepository) checkCommonAndData(db *gorm.DB, orgId string, data *m
 	return nil
 }
 
-func (r *domainRepository) checkCommonAndDataAndType(db *gorm.DB, orgId string, data *model.Domain) error {
-	if err := r.checkCommonAndData(db, orgId, data); err != nil {
+func (r *domainRepository) checkCommonAndDataAndType(
+	db *gorm.DB,
+	orgID string,
+	data *model.Domain,
+) error {
+	if err := r.checkCommonAndData(db, orgID, data); err != nil {
 		return err
 	}
 	if data.Type == nil {
 		return fmt.Errorf("'Type' is nil")
 	}
+	return nil
+}
+
+func (r *domainRepository) createIpaDomain(
+	db *gorm.DB,
+	domainID uint,
+	data *model.Ipa,
+) (err error) {
+	if data == nil {
+		return fmt.Errorf("'data' of type '*model.Ipa' is nil")
+	}
+	data.Model.ID = domainID
+	tokenExpiration := &time.Time{}
+	*tokenExpiration = time.Now().Add(model.DefaultTokenExpiration()).UTC()
+	data.TokenExpiration = tokenExpiration
+	if err = db.Omit(clause.Associations).Create(data).Error; err != nil {
+		return err
+	}
+	for idx := range data.CaCerts {
+		data.CaCerts[idx].IpaID = data.ID
+		if err = db.Create(&data.CaCerts[idx]).Error; err != nil {
+			return err
+		}
+	}
+	for idx := range data.Servers {
+		data.Servers[idx].IpaID = data.ID
+		if err = db.Create(&data.Servers[idx]).Error; err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *domainRepository) updateIpaDomain(
+	db *gorm.DB,
+	data *model.Ipa,
+) (err error) {
+	if data == nil {
+		return fmt.Errorf("'data' of type '*model.Ipa' is nil")
+	}
+	if err = db.Unscoped().
+		Delete(
+			data,
+			"id = ?",
+			data.ID,
+		).Error; err != nil {
+		return err
+	}
+
+	data.Token = nil
+	data.TokenExpiration = nil
+	if err = db.Omit(clause.Associations).
+		Create(data).
+		Error; err != nil {
+		return err
+	}
+
+	// CaCerts
+	for i := range data.CaCerts {
+		data.CaCerts[i].IpaID = data.ID
+		if err = db.Create(&data.CaCerts[i]).Error; err != nil {
+			return err
+		}
+	}
+
+	// Servers
+	for i := range data.CaCerts {
+		data.Servers[i].IpaID = data.ID
+		if err = db.Create(&data.Servers[i]).Error; err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
