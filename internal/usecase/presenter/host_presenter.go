2ae112b6042ac582a75de56e54e2fe3bb079b1e0
Christian Heimes <cheimes@redhat.com>
2023-07-20 08:30:21 +0200

    HMS-2147 feat: HostConf presenter
    Implement `HostConf` presenter for `rhel-idm` domain type.
    
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- internal/usecase/presenter/host_presenter.go
+++ internal/usecase/presenter/host_presenter.go
@@ -2,6 +2,7 @@ package presenter
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hmsidm/internal/api/public"
 	"github.com/hmsidm/internal/config"
@@ -20,6 +21,64 @@ func NewHostPresenter(cfg *config.Config) presenter.HostPresenter {
 	return &hostPresenter{cfg}
 }
 
+func (p *hostPresenter) fillRhelIdm(domain *model.Domain, response *public.HostConfResponse) error {
+	// concatenate PEM certs
+	if len(domain.IpaDomain.CaCerts) == 0 {
+		return fmt.Errorf("domain '%s' has no CA certificates", *domain.DomainName)
+	}
+	var sb strings.Builder
+	for _, ca_cert := range domain.IpaDomain.CaCerts {
+		sb.WriteString(ca_cert.Pem)
+		// ensure PEM blocks are separated by newline
+		if !strings.HasSuffix(ca_cert.Pem, "\n") {
+			sb.WriteString("\n")
+		}
+	}
+
+	// create array of servers with HCC enrollment role
+	var servers []public.HostConfIpaServer
+	for _, ipa_server := range domain.IpaDomain.Servers {
+		if ipa_server.HCCEnrollmentServer {
+			servers = append(servers, public.HostConfIpaServer{Fqdn: ipa_server.FQDN, Location: ipa_server.Location})
+		}
+	}
+	if len(servers) == 0 {
+		return fmt.Errorf("domain '%s' has no enrollment servers", *domain.DomainName)
+	}
+	response.RhelIdm = public.HostConfIpa{
+		Cabundle:          sb.String(),
+		EnrollmentServers: servers,
+		RealmName:         *domain.IpaDomain.RealmName,
+	}
+	return nil
+}
+
 func (p *hostPresenter) HostConf(domain *model.Domain) (*public.HostConfResponse, error) {
-	return nil, fmt.Errorf("TODO: not implemented")
+	var err error
+
+	if domain == nil {
+		return nil, fmt.Errorf("'domain' is nil")
+	}
+	if domain.Type == nil {
+		return nil, fmt.Errorf("'domain.Type' is nil")
+	}
+	domainType := public.DomainType(model.DomainTypeString(*domain.Type))
+
+	response := &public.HostConfResponse{
+		AutoEnrollmentEnabled: *domain.AutoEnrollmentEnabled,
+		DomainId:              domain.DomainUuid,
+		DomainName:            *domain.DomainName,
+		DomainType:            domainType,
+	}
+
+	switch *domain.Type {
+	case model.DomainTypeIpa:
+		err = p.fillRhelIdm(domain, response)
+	default:
+		err = fmt.Errorf("domain '%s' has unsupported domain type '%s'", *domain.DomainName, domainType)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
