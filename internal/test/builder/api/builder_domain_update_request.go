30e9bb9835276baee137fa45475cb0910ff9b343
Alejandro Visiedo <avisiedo@redhat.com>
2023-12-13 17:13:25 +0100

    HMS-3156 test: add domain update agent
    This change add the builder for updating a domain by an agent and a smoke test
    where the success scenario works correctly.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

---
+++ internal/test/builder/api/builder_domain_update_request.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"github.com/google/uuid"
+	"github.com/podengo-project/idmsvc-backend/internal/api/public"
+)
+
+type UpdateDomainAgent interface {
+	Build() *public.UpdateDomainAgentRequest
+	WithDomainName(value string) UpdateDomainAgent
+	WithDomainType(value public.DomainType) UpdateDomainAgent
+	WithDomainRhelIdm(value public.DomainIpa) UpdateDomainAgent
+	WithSubscriptionManagerID(value string) UpdateDomainAgent
+	WithHCCUpdate(value bool) UpdateDomainAgent
+}
+
+type updateDomainAgentRequest public.UpdateDomainAgentRequest
+
+func NewUpdateDomainAgent(domainName string) UpdateDomainAgent {
+	return &updateDomainAgentRequest{
+		DomainName: domainName,
+		DomainType: public.RhelIdm,
+		RhelIdm:    *NewRhelIdmDomain(domainName).Build(),
+	}
+}
+
+func (b *updateDomainAgentRequest) Build() *public.UpdateDomainAgentJSONRequestBody {
+	return (*public.UpdateDomainAgentRequest)(b)
+}
+
+func (b *updateDomainAgentRequest) WithDomainName(value string) UpdateDomainAgent {
+	return b
+}
+
+func (b *updateDomainAgentRequest) WithDomainType(value public.DomainType) UpdateDomainAgent {
+	return b
+}
+
+func (b *updateDomainAgentRequest) WithDomainRhelIdm(value public.DomainIpa) UpdateDomainAgent {
+	return b
+}
+
+func (b *updateDomainAgentRequest) WithSubscriptionManagerID(value string) UpdateDomainAgent {
+	if len(b.RhelIdm.Servers) > 0 {
+		*b.RhelIdm.Servers[0].SubscriptionManagerId = uuid.MustParse(value)
+	}
+	return b
+}
+
+func (b *updateDomainAgentRequest) WithHCCUpdate(value bool) UpdateDomainAgent {
+	if len(b.RhelIdm.Servers) > 0 {
+		b.RhelIdm.Servers[0].HccUpdateServer = value
+	}
+	return b
+}
