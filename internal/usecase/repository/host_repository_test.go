3c6cb721119eea9b56ad2ec8fa3e8267ea6ea553
Alejandro Visiedo <avisiedo@redhat.com>
2024-02-05 22:47:29 +0100

    test(HMS-3507): partial cover SignHostConfToken
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/usecase/repository/host_repository_test.go
+++ internal/usecase/repository/host_repository_test.go
@@ -183,6 +183,72 @@ func (s *SuiteHost) TestMatchDomain() {
 	require.NoError(t, err)
 }
 
+func (s *SuiteHost) TestSignHostConfToken() {
+	t := s.Suite.T()
+	domainName := helper.GenRandDomainName(2)
+	fqdn := helper.GenRandFQDNWithDomain(domainName)
+	domainId := uuid.New()
+	options := &interactor.HostConfOptions{
+		OrgId:       "12345",
+		CommonName:  domainId,
+		InventoryId: uuid.MustParse("9db10f12-c421-11ee-8c1c-482ae3863d30"),
+		Fqdn:        fqdn,
+		DomainId:    &domainId,
+		DomainName:  &domainName,
+		DomainType:  (*api_public.DomainType)(pointy.String(model.DomainTypeIpaString)),
+	}
+	id := uint(helper.GenRandNum(0, 2^63))
+	realm := strings.ToUpper(domainName)
+	domain := *builder_model.NewDomain(builder_model.NewModel().WithID(id).Build()).
+		WithOrgID(options.OrgId).
+		WithAutoEnrollmentEnabled(pointy.Bool(true)).
+		WithIpaDomain(
+			builder_model.NewIpaDomain().
+				WithModel(builder_model.NewModel().WithID(id).Build()).
+				WithRealmName(&realm).
+				WithRealmDomains(pq.StringArray{domainName}).
+				WithServers([]model.IpaServer{
+					builder_model.NewIpaServer(
+						builder_model.NewModel().Build(),
+					).WithIpaID(id).
+						WithFQDN(fqdn).
+						Build(),
+				}).
+				WithLocations([]model.IpaLocation{
+					builder_model.NewIpaLocation(
+						builder_model.NewModel().Build(),
+					).WithIpaID(id).Build(),
+				}).
+				WithCaCerts([]model.IpaCert{
+					builder_model.NewIpaCert(
+						builder_model.NewModel().Build(),
+						realm,
+					).WithIpaID(id).Build(),
+				}).
+				Build(),
+		).Build()
+
+	// guard options is nil
+	token, err := s.repository.SignHostConfToken(nil, nil, nil)
+	assert.Equal(t, "", token)
+	require.EqualError(t, err, "code=500, message='options' cannot be nil")
+
+	// guard domain is nil
+	token, err = s.repository.SignHostConfToken(nil, options, nil)
+	assert.Equal(t, "", token)
+	require.EqualError(t, err, "code=500, message='domain' cannot be nil")
+
+	// no signers available
+	token, err = s.repository.SignHostConfToken(nil, options, &domain)
+	assert.Equal(t, "", token)
+	require.EqualError(t, err, "jws.Sign: no signers available. Specify an alogirthm and akey using jws.WithKey()")
+
+	// no signers
+	token, err = s.repository.SignHostConfToken(nil, options, &domain)
+	assert.Equal(t, "", token)
+	require.EqualError(t, err, "jws.Sign: no signers available. Specify an alogirthm and akey using jws.WithKey()")
+}
+
 func TestSuiteHost(t *testing.T) {
 	suite.Run(t, new(SuiteHost))
 }
