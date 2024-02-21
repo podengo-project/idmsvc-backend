f39d78c0f7617e41957d62daa9b6b33670131d96
Christian Heimes <cheimes@redhat.com>
2024-02-21 10:47:44 +0100

    refactor: Remove unnecessary BeforeCreate hook
    There is no need to populate CreatedAt and UpdatedAt fields with a
    custom hook. Gorm maintains the fields automatically for us.
    
    See: https://gorm.io/docs/models.html#gorm-Model
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- internal/infrastructure/token/hostconf_jwk/model/hostconf_jwk_model_test.go
+++ internal/infrastructure/token/hostconf_jwk/model/hostconf_jwk_model_test.go
@@ -13,16 +13,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBeforeCreateHook(t *testing.T) {
-	item := &HostconfJwk{}
-	assert.Empty(t, item.CreatedAt)
-	assert.Empty(t, item.UpdatedAt)
-
-	item.BeforeCreate(nil)
-	assert.NotEmpty(t, item.CreatedAt)
-	assert.NotEmpty(t, item.UpdatedAt)
-}
-
 func TestNewHostconfJwk(t *testing.T) {
 	config := test.GetTestConfig()
 	expiresAt := time.
