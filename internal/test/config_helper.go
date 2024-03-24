75bfac2b64c7a1c525f97e64c29ab7f83c12e2e4
Alejandro Visiedo <avisiedo@redhat.com>
2024-03-24 20:38:52 +0100

    test(HMS-3527): integrate rbac mock with smoke tests
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/test/config_helper.go
+++ internal/test/config_helper.go
@@ -1,12 +1,17 @@
 package test
 
 import (
+	"os"
+
 	"github.com/podengo-project/idmsvc-backend/internal/config"
 	"github.com/podengo-project/idmsvc-backend/internal/infrastructure/secrets"
 )
 
 // Config for testing
 func GetTestConfig() (cfg *config.Config) {
+	if cfgpath, ok := os.LookupEnv("CONFIG_PATH"); !ok || cfgpath == "" {
+		panic("CONFIG_PATH is not set or is empty")
+	}
 	cfg = &config.Config{}
 	config.Load(cfg)
 
@@ -17,12 +22,6 @@ func GetTestConfig() (cfg *config.Config) {
 	cfg.Application.HostconfJwkRenewalThreshold = config.DefaultHostconfJwkRenewalThreshold
 	cfg.Application.PaginationDefaultLimit = 10
 	cfg.Application.PaginationMaxLimit = 100
-	// initialize secrets
-	sec, err := secrets.NewAppSecrets(cfg.Application.MainSecret)
-	if err != nil {
-		panic(err)
-	}
-	cfg.Secrets = *sec
 
 	return cfg
 }
