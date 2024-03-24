7deaa605d86568c0d28ff085cfd270d33dfd1975
Alejandro Visiedo <avisiedo@redhat.com>
2024-03-24 20:25:28 +0100

    feat(HMS-3527): add rbac mock service
    Add rbac service implementation which provides four
    different profiles to check our needs:
    - super-admin.yaml: it only contains the wildcard entry
      for checking they are processed correctly.
    - domain-admin.yaml: it contains all the permissions
      that would have a domain administrator role.
    - domain-readonly.yaml: it contains the read permissions.
    - domain-none.yaml: it contains none permissions, so every
      access to the API with this one should fails.
    - custom.yaml: it is provided to be updated by the
      developer as it needs for checking different scenarios;
      it could be useful for debugging purpose.
    
    Add support to debug with rbac mock from vscode.
    
    Thanks to the reviewers for providing feedback to finetune
    the changes.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>
    Co-authored-by: Fraser Tweedalie <ftweedal@redhat.com>
    Co-authored-by: Petr Vobornik <pvoborni@redhat.com>

---
+++ internal/infrastructure/service/impl/mock/rbac/impl/rbac.go
@@ -0,0 +1,242 @@
+package impl
+
+import (
+	"context"
+	_ "embed"
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/podengo-project/idmsvc-backend/internal/config"
+	"github.com/podengo-project/idmsvc-backend/internal/infrastructure/service"
+	"golang.org/x/exp/slog"
+	"gopkg.in/yaml.v3"
+)
+
+var (
+	errRbacMockAwaitTimeout = errors.New("timeout awaiting rbac mock to be ready")
+	errRbacMockUnknown      = errors.New("unknown error happened on rbac mock")
+)
+
+//go:embed super-admin.yaml
+var rbacProfileSuperAdmin []byte
+
+//go:embed domain-admin.yaml
+var rbacProfileAdmin []byte
+
+//go:embed domain-readonly.yaml
+var rbacProfileReadOnly []byte
+
+//go:embed domain-none.yaml
+var rbacProfileNoPerms []byte
+
+//go:embed custom.yaml
+var rbacProfileCustom []byte
+
+const (
+	ProfileSuperAdmin     = "super-admin"
+	ProfileDomainAdmin    = "domain-admin"
+	ProfileDomainReadOnly = "domain-readonly"
+	ProfileDomainNoPerms  = "domain-no-perms"
+	ProfileCustom         = "custom"
+)
+
+var Profiles map[string][]string = map[string][]string{
+	ProfileSuperAdmin:     LoadProfile(rbacProfileSuperAdmin),
+	ProfileDomainAdmin:    LoadProfile(rbacProfileAdmin),
+	ProfileDomainReadOnly: LoadProfile(rbacProfileReadOnly),
+	ProfileDomainNoPerms:  LoadProfile(rbacProfileNoPerms),
+	ProfileCustom:         LoadProfile(rbacProfileCustom),
+}
+
+// https://consoledot.pages.redhat.com/docs/dev/services/rbac.html#_retrieve_and_handle_access_list
+type Page struct {
+	Meta  map[string]any    `json:"meta"`
+	Links map[string]string `json:"links"`
+	Data  []Permission      `json:"data"`
+}
+
+type Permission struct {
+	Permission          string `json:"permission"`
+	ResourceDefinitions []any  `json:"resourceDefinitions"`
+}
+
+type MockRbac interface {
+	SetPermissions(data []string)
+	GetBaseURL() string
+	WaitAddress(timeout time.Duration) error
+}
+
+type mockRbac struct {
+	echo       *echo.Echo
+	lock       sync.Mutex
+	context    context.Context
+	cancelFunc context.CancelFunc
+	address    string
+	waitGroup  *sync.WaitGroup
+	port       string
+	data       []Permission
+}
+
+// LoadProfile unmarshall a yaml content with a list
+// of permissions to let to externalize the static
+// contents.
+// Return the list of strings, but currently no checks
+// are made currently.
+func LoadProfile(data []byte) []string {
+	result := []string{}
+	err := yaml.Unmarshal(data, &result)
+	if err != nil {
+		panic(err.Error())
+	}
+	// TODO The data are not being validated:
+	// - every string is a tuple of 3 items separated by ':'.
+	// - first item must match the service id, for instance 'idmsvc'.
+	// - second item must match the allowed resources by the service.
+	// - third item must match a valid verb.
+	return result
+}
+
+func newRbacMockGuards(ctx context.Context, cfg *config.Config) {
+	if ctx == nil {
+		panic("ctx is nil")
+	}
+	if cfg == nil {
+		panic("cfg is nil")
+	}
+	if !cfg.Application.EnableRBAC {
+		panic("Config.Application.EnableRBAC is false")
+	}
+	if cfg.Clients.RbacBaseURL == "" {
+		panic("Config.Clients.RbacBaseURL is an empty string")
+	}
+}
+
+// NewRbacMock return a new rbac mock service for testing.
+func NewRbacMock(ctx context.Context, cfg *config.Config) (service.ApplicationService, MockRbac) {
+	var (
+		cancelFunc  context.CancelFunc
+		profileName string
+	)
+	urlData, err := url.Parse(cfg.Clients.RbacBaseURL)
+	if err != nil {
+		panic(fmt.Sprintf("error parsing rbac client url: %s", err.Error()))
+	}
+	address := fmt.Sprintf("%s:%s", urlData.Hostname(), urlData.Port())
+	ctx, cancelFunc = context.WithCancel(ctx)
+	e := echo.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+	m := &mockRbac{
+		address:    address,
+		echo:       e,
+		context:    ctx,
+		cancelFunc: cancelFunc,
+		waitGroup:  &sync.WaitGroup{},
+		lock:       sync.Mutex{},
+	}
+	e.GET(fmt.Sprintf("%s/access", urlData.Path), m.accessHandler)
+	profileName = os.Getenv("APP_CLIENTS_RBAC_PROFILE")
+	if profileName == "" {
+		profileName = ProfileDomainAdmin
+	}
+	if profileData, ok := Profiles[profileName]; ok {
+		m.SetPermissions(profileData)
+	} else {
+		slog.Error("not found", "profile_name", profileName)
+		panic("rbac mock profile not found")
+	}
+	return m, m
+}
+
+func (m *mockRbac) Start() error {
+	m.echo.HideBanner = true
+	m.echo.Debug = false
+	m.echo.HidePort = false
+	m.waitGroup.Add(2)
+	go func() {
+		defer m.waitGroup.Done()
+		slog.Info("mock rbac service starting")
+		if err := m.echo.Start(m.address); err != nil {
+			slog.Error(err.Error())
+			return
+		}
+	}()
+	go func() {
+		defer m.waitGroup.Done()
+		defer m.cancelFunc()
+		<-m.context.Done()
+		if err := m.echo.Shutdown(m.context); err != nil {
+			slog.Error(err.Error())
+			return
+		}
+	}()
+	return nil
+}
+
+func (m *mockRbac) Stop() error {
+	slog.Info("mock rback service stopping")
+	defer m.waitGroup.Wait()
+	m.cancelFunc()
+	return nil
+}
+
+// SetPermissions allow to dynamically assign the list
+// of permissions that will return the mock service
+// when it is reached out for accessin the acl list.
+// data contains the information to be returned.
+func (m *mockRbac) SetPermissions(data []string) {
+	newData := make([]Permission, len(data))
+	for i := range data {
+		newData[i] = Permission{
+			Permission:          data[i],
+			ResourceDefinitions: []any{},
+		}
+	}
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	m.data = newData
+}
+
+// WaitAddress is a naive implementation to await the rbac
+// mock has an address assigned.
+func (m *mockRbac) WaitAddress(timeout time.Duration) error {
+	isListening := false
+	deadline := time.Now().Add(timeout)
+	for time.Now().Compare(deadline) < 0 {
+		if m.echo.Listener != nil && m.echo.Listener.Addr().String() != "" {
+			slog.Info(fmt.Sprintf("rbac mock listening at: %s", m.echo.Listener.Addr().String()))
+			isListening = true
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	if !isListening {
+		if time.Now().Compare(deadline) >= 0 {
+			return errRbacMockAwaitTimeout
+		} else {
+			return errRbacMockUnknown
+		}
+	}
+	return nil
+}
+
+// GetBaseURL retrieve the base URL to reach out the
+// rbac mock.
+// Return empty string if the listener is not yet assigned;
+// see WaitAddress method.
+func (m *mockRbac) GetBaseURL() string {
+	addr := ""
+	if m.echo.Listener != nil {
+		addr = m.echo.Listener.Addr().String()
+		if addr != "" {
+			return fmt.Sprintf("http://%s/api/rbac/v1", addr)
+		}
+	}
+
+	return addr
+}
---
+++ internal/infrastructure/service/impl/mock/rbac/impl/rbac_test.go
@@ -0,0 +1,160 @@
+package impl
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/podengo-project/idmsvc-backend/internal/config"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func helperConfig() *config.Config {
+	cfg := &config.Config{}
+	config.Load(cfg)
+
+	// Override config values
+	cfg.Application.EnableRBAC = true
+	if cfg.Clients.RbacBaseURL == "" {
+		panic("when rbac is enabled, set your 'clients.rbac_base_url' at your 'congis/config.yaml' file or CLIENTS_RBAC_BASE_URL variable to override")
+	}
+	return cfg
+}
+
+func TestNewMockRbacGuards(t *testing.T) {
+	assert.PanicsWithValue(t, "ctx is nil", func() {
+		newRbacMockGuards(nil, nil)
+	})
+
+	ctx := context.Background()
+	assert.PanicsWithValue(t, "cfg is nil", func() {
+		newRbacMockGuards(ctx, nil)
+	})
+
+	cfg := &config.Config{}
+	assert.PanicsWithValue(t, "Config.Application.EnableRBAC is false", func() {
+		newRbacMockGuards(ctx, cfg)
+	})
+
+	cfg.Application.EnableRBAC = true
+	assert.PanicsWithValue(t, "Config.Clients.RbacBaseURL is an empty string", func() {
+		newRbacMockGuards(ctx, cfg)
+	})
+
+}
+
+func TestNewMockRbac(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cfg := &config.Config{}
+	cfg.Application.EnableRBAC = true
+	cfg.Clients.RbacBaseURL = "\n"
+	assert.PanicsWithValue(t, "error parsing rbac client url: parse \"\\n\": net/url: invalid control character in URL", func() {
+		NewRbacMock(ctx, cfg)
+	})
+	cancel()
+
+	// Success scenario
+	ctx, cancel = context.WithCancel(context.Background())
+	cfg = helperConfig()
+	assert.NotPanics(t, func() {
+		NewRbacMock(ctx, cfg)
+	})
+	cancel()
+}
+
+func TestStartStop(t *testing.T) {
+	var err error
+	cfg := helperConfig()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	srv, _ := NewRbacMock(ctx, cfg)
+	assert.NotPanics(t, func() {
+		err = srv.Start()
+	})
+	require.NoError(t, err)
+
+	assert.NotPanics(t, func() {
+		err = srv.Stop()
+	})
+	require.NoError(t, err)
+}
+
+func TestGetBaseURL(t *testing.T) {
+	var err error
+	cfg := helperConfig()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	srv, mock := NewRbacMock(ctx, cfg)
+	require.NotNil(t, srv)
+	require.NotNil(t, mock)
+	err = srv.Start()
+	require.NoError(t, err)
+	defer srv.Stop()
+	err = mock.WaitAddress(3 * time.Second)
+	require.NoError(t, err)
+
+	assert.NotEqual(t, "", mock.GetBaseURL())
+}
+
+func isStringInSlice(str string, slice []string) bool {
+	if slice == nil {
+		return false
+	}
+	for i := range slice {
+		if str == slice[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func EqualStringSlices(t *testing.T, expected, current []string) bool {
+	for i := range expected {
+		if !isStringInSlice(expected[i], current) {
+			t.Logf("expected '%s' string not found into the current slice", expected[i])
+			return false
+		}
+	}
+
+	for i := range current {
+		if !isStringInSlice(current[i], expected) {
+			t.Logf("current '%s' string not found into the expected slice", current[i])
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestLoadProfile(t *testing.T) {
+	assert.PanicsWithValue(t, "yaml: unmarshal errors:\n  line 1: cannot unmarshal !!str `--` into []string", func() {
+		LoadProfile([]byte(`--`))
+	}, "Panic on unmarshalling the yaml data")
+
+	LoadProfile([]byte(`---
+- idmsvc:resource:read
+`))
+}
+
+func TestProfiles(t *testing.T) {
+	profiles := []string{
+		ProfileSuperAdmin,
+		ProfileDomainAdmin,
+		ProfileDomainReadOnly,
+		ProfileDomainNoPerms,
+		ProfileCustom,
+	}
+
+	// forward reference
+	for _, profile := range profiles {
+		_, ok := Profiles[profile]
+		require.True(t, ok)
+	}
+
+	// reverse reference
+	for profile := range Profiles {
+		require.True(t, isStringInSlice(profile, profiles))
+	}
+}
