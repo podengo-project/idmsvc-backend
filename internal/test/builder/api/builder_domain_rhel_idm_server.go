6524c7377a398859de6fe8919ca588a1827f9a87
Alejandro Visiedo <avisiedo@redhat.com>
2024-04-01 00:24:04 +0200

    test(HMS-3523): update builders
    - Add the necessary builder methods for a NewServer.
    - Refactor the UpdateDomainUserJSONRequestBody builder
      to match the right type name UpdateDomainUserRequest.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/test/builder/api/builder_domain_rhel_idm_server.go
+++ internal/test/builder/api/builder_domain_rhel_idm_server.go
@@ -10,6 +10,8 @@ type DomainIpaServer interface {
 	Build() public.DomainIpaServer
 	// TODO Add methods and implement them as they are needed
 	// With...() DomainIpaServer
+	WithHccUpdateServer(value bool) DomainIpaServer
+	WithSubscriptionManagerId(value string) DomainIpaServer
 }
 
 type domainIpaServer public.DomainIpaServer
@@ -26,6 +28,18 @@ func NewDomainIpaServer(fqdn string) DomainIpaServer {
 		Location:              nil,
 		SubscriptionManagerId: subscriptionManagerID,
 	}
+}
+
+func (b *domainIpaServer) WithHccUpdateServer(value bool) DomainIpaServer {
+	b.HccUpdateServer = value
+	return b
+}
+
+func (b *domainIpaServer) WithSubscriptionManagerId(value string) DomainIpaServer {
+	subscriptionManagerID := &uuid.UUID{}
+	*subscriptionManagerID = uuid.MustParse(value)
+	b.SubscriptionManagerId = subscriptionManagerID
+	return b
 }
 
 func (b *domainIpaServer) Build() public.DomainIpaServer {
