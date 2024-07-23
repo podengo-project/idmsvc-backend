1ebb5def97f43a1fba90294f7d036d47d9ba1199
Alejandro Visiedo <avisiedo@redhat.com>
2024-07-23 18:39:37 +0200

    feat(HMS-4276): add contextual log to rbac mock
    This change add the contextual log to the rbac
    mock router as it uses the same logger middleware
    which make it a requirement.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/infrastructure/service/impl/mock/rbac/impl/rbac.go
+++ internal/infrastructure/service/impl/mock/rbac/impl/rbac.go
@@ -16,6 +16,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/podengo-project/idmsvc-backend/internal/config"
 	"github.com/podengo-project/idmsvc-backend/internal/infrastructure/logger"
+	app_middleware "github.com/podengo-project/idmsvc-backend/internal/infrastructure/middleware"
 	"github.com/podengo-project/idmsvc-backend/internal/infrastructure/service"
 	"gopkg.in/yaml.v3"
 )
@@ -134,6 +135,7 @@ func NewRbacMock(ctx context.Context, cfg *config.Config) (service.ApplicationSe
 	ctx, cancelFunc = context.WithCancel(ctx)
 	e := echo.New()
 	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(app_middleware.ContextLogConfig(&app_middleware.LogConfig{}))
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
 		// Request logger values for middleware.RequestLoggerValues
 		LogError:  true,
