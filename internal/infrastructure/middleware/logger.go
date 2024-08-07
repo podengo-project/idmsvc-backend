67643ab27db43f47053390fab765faf028498030
Alejandro Visiedo <avisiedo@redhat.com>
2024-08-07 12:50:54 +0200

    feat(HMS-4545): add source ip to logs
    This change add the source_ip aggregated field to the logs.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- internal/infrastructure/middleware/logger.go
+++ internal/infrastructure/middleware/logger.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"log/slog"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,6 +16,15 @@ const (
 
 type LogConfig struct {
 	Skipper middleware.Skipper
+}
+
+func getSource(xForwardedFor string) string {
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
+	source := ""
+	if xForwardedFor != "" {
+		source = strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	}
+	return source
 }
 
 func ContextLogConfig(cfg *LogConfig) echo.MiddlewareFunc {
@@ -31,11 +41,13 @@ func ContextLogConfig(cfg *LogConfig) echo.MiddlewareFunc {
 			}
 
 			requestID := c.Request().Header.Get(header.HeaderXRequestID)
+			source := getSource(c.Request().Header.Get(header.HeaderXForwardedFor))
 
 			// Let print the request-id for every call of the
 			// request log
 			logger := slog.Default().With(
 				slog.String("request_id", requestID),
+				slog.String("source_host", source),
 			)
 			ctx := c.Request().Context()
 			ctx = app_context.CtxWithLog(ctx, logger)
