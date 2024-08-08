6093fd178681c9d7ce3111753799185ec5239308
Alejandro Visiedo <avisiedo@redhat.com>
2024-08-08 13:41:55 +0200

    feat(HMS-4532): add pendo client
    This change add a pendo client which send the metric values.
    
    https://issues.redhat.com/browse/HMS-4532
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

---
+++ internal/usecase/client/pendo/pendo_client_dump.go
@@ -0,0 +1,54 @@
+package pendo
+
+import (
+	"context"
+	"log/slog"
+
+	app_context "github.com/podengo-project/idmsvc-backend/internal/infrastructure/context"
+	"github.com/podengo-project/idmsvc-backend/internal/interface/client/pendo"
+)
+
+type pendoClientDump struct{}
+
+// NewClientDump
+func NewClientDump() pendo.Pendo {
+	return newClientDump()
+}
+
+// SetMetadata launch a request to pendo service to register some metric
+func (c *pendoClientDump) SetMetadata(ctx context.Context, kind pendo.Kind, group pendo.Group, metrics pendo.SetMetadataRequest) (*pendo.SetMetadataResponse, error) {
+	logger := app_context.LogFromCtx(ctx)
+	logger.Debug("pendo SetMetadata called",
+		slog.String("kind", string(kind)),
+		slog.String("group", string(group)),
+	)
+	metaDataResponse := &pendo.SetMetadataResponse{
+		Total:   int64(len(metrics)),
+		Updated: int64(len(metrics)),
+		Failed:  0,
+		Kind:    kind,
+	}
+	logger.Debug("pendo SetMetadata returning",
+		slog.Int64("total", metaDataResponse.Total),
+		slog.Int64("failed", metaDataResponse.Failed),
+		slog.Int64("updated", metaDataResponse.Updated),
+		slog.Any("missing", metaDataResponse.Missing),
+		slog.String("kind", string(metaDataResponse.Kind)),
+	)
+	return metaDataResponse, nil
+}
+
+// SendTrackEvent
+func (c *pendoClientDump) SendTrackEvent(ctx context.Context, track *pendo.TrackRequest) error {
+	logger := app_context.LogFromCtx(ctx)
+	logger.Debug("pendo SendTrackEvent called")
+	return nil
+}
+
+//
+// ----- Private methods ------
+//
+
+func newClientDump() *pendoClientDump {
+	return &pendoClientDump{}
+}
