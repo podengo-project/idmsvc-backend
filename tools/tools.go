987bc8f1c62a1b40198ff5c2850151f88a8b25a1
Fraser Tweedale <ftweedal@redhat.com>
2025-06-13 13:52:03 +1000

    chore(deps): update github.com/golangci/golangci-lint to v2.1.6

--- tools/tools.go
+++ tools/tools.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/achiku/planter"
 	_ "github.com/atombender/go-jsonschema"
 	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/loov/goda"
 	_ "github.com/mikefarah/yq/v4"
 	_ "github.com/spf13/cobra-cli"
